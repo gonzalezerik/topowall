777c7994c968c8ebda9c7744b5f5c04623e0baa6
Erik Gonzalez <dev@gonzalezerik.com>
2026-09-13 19:10:26 -0700

    Release 0.2.1

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       packages = forAll (pkgs: rec {
         topowall = pkgs.rustPlatform.buildRustPackage {
           pname = "topowall";
-          version = "0.2.0";
+          version = "0.2.1";
           src = self;
           cargoLock.lockFile = ./Cargo.lock;
           nativeBuildInputs = [ pkgs.makeWrapper ];
