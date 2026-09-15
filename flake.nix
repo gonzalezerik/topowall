65d633b8f99bd5a11696200915f50130d6971890
Erik Gonzalez <dev@gonzalezerik.com>
2026-09-15 00:20:44 -0700

    Bump version to 0.2.2

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       packages = forAll (pkgs: rec {
         topowall = pkgs.rustPlatform.buildRustPackage {
           pname = "topowall";
-          version = "0.2.1";
+          version = "0.2.2";
           src = self;
           cargoLock.lockFile = ./Cargo.lock;
           nativeBuildInputs = [ pkgs.makeWrapper ];
